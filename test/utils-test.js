ddc0485ac4274cce816744fb3b361a3433a9818a
Juan Pablo <jpbaena13@gmail.com>
2017-08-25 16:32:19 -0500

    Pruebas con ava

---
+++ test/utils-test.js
@@ -0,0 +1,27 @@
+'use strict'
+
+const test = require('ava')
+
+const utils = require('../lib/utils')
+
+test('extracting hashtag from text', t => {
+  let tags = utils.extractTags('a #picture with tags #AwEsOmE #Platzi #AVA and #100 ##yes')
+
+  t.deepEqual(tags, [
+    'picture',
+    'awesome',
+    'platzi',
+    'ava',
+    '100',
+    'yes'
+  ])
+
+  tags = utils.extractTags('a picture with no tags')
+  t.deepEqual(tags, [])
+
+  tags = utils.extractTags()
+  t.deepEqual(tags, [])
+
+  tags = utils.extractTags(null)
+  t.deepEqual(tags, [])
+})
